dcf3977314e16b732d67bc8b8892d9c4c0e5fa16
Nicolas Debray <nicdebray@gmail.com>
2018-12-23 21:28:12 +0100

    added the students model, controller, views and references batches

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -2,24 +2,44 @@ class StudentsController < ApplicationController
   before_action :student_find, only: [:show, :edit, :update, :destroy]
 
   def index
+    @students = Student.all
   end
 
   def show
   end
 
   def new
+    @student = Student.new
   end
 
   def create
+    @batch = Batch.find(Batch.last.id)
+    @student = Student.new(student_params)
+    @student.batch = Batch.find(Batch.last.id)
+    if @student.save
+      redirect_to students_path
+    else
+      render :new
+    end
   end
 
   def edit
   end
 
   def update
+    if @student.update(student_params)
+      redirect_to students_path
+    else
+      render :new
+    end
   end
 
   def destroy
+    if @student.destroy
+      redirect_to students_path
+    else
+      render :new
+    end
   end
 
   private
@@ -28,6 +48,7 @@ class StudentsController < ApplicationController
   end
 
   def student_find
+    @batch = Batch.find(Batch.last.id)
     @student = Student.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
-  get 'students/index'
-  get 'students/show'
+  get 'students', to:'students#index'
   get 'students/new'
-  get 'students/create'
-  get 'students/edit'
-  get 'students/update'
-  get 'students/destroy'
+  get 'students/:id', to:'students#show', as: :student
+  post 'students', to:'students#create'
+  get 'students/:id/edit', to:'students#edit', as: :edit_student
+  patch 'students/:id', to:'students#update'
+  delete 'students/:id', to:'students#destroy'
+
   get 'batches', to:'batches#index'
   get 'batches/new'
   get 'batches/:id', to:'batches#show', as: :batch
@@ -13,6 +14,8 @@ Rails.application.routes.draw do
   get 'batches/:id/edit', to:'batches#edit', as: :edit_batch
   patch 'batches/:id', to:'batches#update'
   delete 'batches/:id', to:'batches#destroy'
+
   get 'pages/home'
+
   root to: 'pages#home'
 end
