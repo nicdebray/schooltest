b4b2f088ed511244f3688b2d0e9931fe3feed4a0
Nicolas Debray <nicdebray@gmail.com>
2019-01-03 23:11:33 +0100

    made further changes to facilitate the human reading of the index pages

--- app/models/student.rb
+++ app/models/student.rb
@@ -4,6 +4,6 @@ class Student < ApplicationRecord
   has_many :courses, through: :memberships
   has_many :evaluations
 
-  accepts_nested_attributes_for :memberships
+  # accepts_nested_attributes_for :memberships
 
 end
