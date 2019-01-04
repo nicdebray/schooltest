a8f385dc023ffbeb0286cc3ab0ab12d2af946ecb
Nicolas Debray <nicdebray@gmail.com>
2019-01-04 14:24:36 +0100

    fixing dependent: :destroy typo making heroku crash

--- app/models/student.rb
+++ app/models/student.rb
@@ -2,7 +2,7 @@ class Student < ApplicationRecord
   belongs_to :batch, optional: true
   has_many :memberships
   has_many :courses, through: :memberships
-  has_many :evaluations, depedent: :destroy
+  has_many :evaluations, dependent: :destroy
 
   # accepts_nested_attributes_for :memberships
 end
