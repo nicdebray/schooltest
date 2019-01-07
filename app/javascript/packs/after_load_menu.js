5cc001254e5495740c1c94bb72c30af01d263f93
Nicolas Debray <nicdebray@gmail.com>
2019-01-07 23:46:28 +0100

    fixed the postgresql bug due to the mac crash, added an avatar dropdown with quick links, redesigned slightly the sign in up forms

--- app/javascript/packs/after_load_menu.js
+++ app/javascript/packs/after_load_menu.js
@@ -1,7 +1,20 @@
+// navbar quicklinks dropdown start
 function showHide(){
   const menu = document.getElementById("quicklinks-menu");
   menu.classList.toggle("hidden");
 }
 
-const logo = document.querySelector(".navbar-quicklinks");
-logo.addEventListener("click", showHide);
+const quickLinks = document.querySelector(".navbar-quicklinks");
+quickLinks.addEventListener("click", showHide);
+// navbar quicklinks dropdown end
+
+
+// avatar dropdown start
+function showHideAvatar(){
+  const avatarMenu = document.getElementById("quicklinks-user-menu");
+  avatarMenu.classList.toggle("hidden");
+}
+
+const avatarMenu = document.querySelector(".navbar-login");
+avatarMenu.addEventListener("click", showHideAvatar);
+// avatar dropdown end
