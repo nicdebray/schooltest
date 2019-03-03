57ea01b748fa70884e2e1c55699bdd993ea5206d
Nicolas Debray <nicdebray@gmail.com>
2019-03-03 21:51:44 +0100

    added a javascript function when teacher new fields are empty

--- app/javascript/packs/after_load_menu.js
+++ app/javascript/packs/after_load_menu.js
@@ -18,3 +18,15 @@ function showHideAvatar(){
 const avatarMenu = document.querySelector(".navbar-login");
 avatarMenu.addEventListener("click", showHideAvatar);
 // avatar dropdown end
+
+
+// raising alert if input blank error message start
+const raiseAlertBlankField = (text) => {
+  if (text === `can't be blank`) {
+    alert(`Inputs can't be blank`);
+  };
+};
+
+const blankText = document.querySelector('.error').innerText;
+document.addEventListener("loaded", raiseAlertBlankField(blankText));
+// raising alert if input blank error message end
